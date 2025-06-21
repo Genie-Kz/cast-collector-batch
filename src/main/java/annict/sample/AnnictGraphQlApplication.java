12f3ae88dbf8751447dbe36747b32d5ed201faa3
Genie-Kz <126337294+Genie-Kz@users.noreply.github.com>
2025-06-21 23:35:02 +0900

    Initil Commit

---
+++ src/main/java/annict/sample/AnnictGraphQlApplication.java
@@ -0,0 +1,13 @@
+package annict.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnictGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnictGraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/annict/sample/AnnictGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package annict.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnictGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
